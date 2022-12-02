85a2ef7765d751fdaeb5ecda0369a240c40e5dbf
Fassty <mikulaspa@gmail.com>
2022-12-02 16:52:00 +0100

    Eval + parsing

--- evaluate.py
+++ evaluate.py
@@ -4,12 +4,12 @@ import subprocess
 
 
 if __name__ == '__main__':
-    with open('run-1_eval.csv', 'w') as handle:
+    with open('run-1_wordnet_slope_eval.csv', 'w') as handle:
         handle.write('scheme,map\n')
-        for f in os.listdir('nolemma'):
-            command = f'./eval/trec_eval -M1000 qrels-train_cs.txt nolemma/{f}'
+        for f in os.listdir('wordnet_slope'):
+            command = f'./eval/trec_eval -M1000 qrels-train_en.txt wordnet_slope/{f}'
             process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
             map_ = output.decode('ascii').split('\n')[5].split()[2]
             map_ = float(map_)
-            handle.write(f'{"_".join(f.split(".")[0].rsplit("_", maxsplit=2)[1:])},{map_:.5f}\n')
+            handle.write(f'{"_".join(f.rsplit(".", maxsplit=1)[0].rsplit("_", maxsplit=2)[1:])},{map_:.5f}\n')
--- inverted_index/parsing.py
+++ inverted_index/parsing.py
@@ -3,11 +3,10 @@ import pickle
 from typing import List
 
 import pandas as pd
+import pickle
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-
-from inverted_index.preprocessing import udpipe_tokenize_lemmatize
 
 
 def _load_xml_to_dicts(document_path: str) -> List[dict]:
@@ -30,29 +29,22 @@ def _load_xml_to_dicts(document_path: str) -> List[dict]:
 
 def load_documents(document_list_file: str) -> pd.DataFrame:
     document_dir = os.path.splitext(document_list_file)[0]
-    with open(document_list_file) as f:
-        document_filenames = [doc.strip() for doc in f]
+    stored_doc_file = f'raw_documents_{document_dir.split("_")[-1]}.pkl'
+    if not os.path.exists(stored_doc_file):
+        with open(document_list_file) as f:
+            document_filenames = [doc.strip() for doc in f]
 
-    documents = []
-    for document_fn in document_filenames:
-        document_path = os.path.join(document_dir, document_fn)
-        documents.extend(_load_xml_to_dicts(document_path))
+        documents = []
+        for document_fn in document_filenames:
+            document_path = os.path.join(document_dir, document_fn)
+            documents.extend(_load_xml_to_dicts(document_path))
 
-    if os.path.exists('udpipe_lemmas.pkl'):
-        with open('udpipe_lemmas.pkl', 'rb') as f:
-            ud_pipe_lemmas = pickle.load(f)
+        with open(stored_doc_file, 'wb') as f:
+            pickle.dump(documents, f)
     else:
-        ud_pipe_lemmas = {}
-    for i, document in tqdm(enumerate(documents)):
-        if document['DOCNO'] in ud_pipe_lemmas:
-            continue
-        lemmas = udpipe_tokenize_lemmatize(document['DATA'])
-        ud_pipe_lemmas[document['DOCNO']] = lemmas
-        if i % 50 == 0:
-            with open('udpipe_lemmas.pkl', 'wb') as f:
-                pickle.dump(ud_pipe_lemmas, f)
-    with open('udpipe_lemmas.pkl', 'wb') as f:
-        pickle.dump(ud_pipe_lemmas, f)
+        with open(stored_doc_file, 'rb') as f:
+            documents = pickle.load(f)
+
     return pd.DataFrame(documents)
 
 
